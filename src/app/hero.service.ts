05e507fb4ba92e43d2e30acfa1d84850724b174a
Asif Khan <asif.k@Technovert.com>
2022-07-11 12:00:55 +0530

    completed the tour of heroes tutorial

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,14 +1,20 @@
 import { Injectable } from '@angular/core';
 import { Hero } from './hero';
 import { HEROES } from './mock-heroes';
-import { Observable, observable, of } from 'rxjs';
+import {  Observable, observable, of } from 'rxjs';
 import { MessageService } from './message.service';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { catchError, map, tap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
+
 export class HeroService {
+ 
+  httpOptions = {
+    headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+  };
   private heroesUrl ='api/heroes';
   constructor(
     private http:HttpClient,
@@ -21,15 +27,40 @@ export class HeroService {
   // }
   getHeroes(): Observable<Hero[]> //in-memory-data
   {
-    return this.http.get<Hero[]>(this.heroesUrl)
+    return this.http.get<Hero[]>(this.heroesUrl);
+    
   }
-  getHero(id: number): Observable<Hero>
-  {
-    const hero = HEROES.find(h=>h.id===id)!;
-    return of(hero);
-  }
+  /** GET hero by id. Will 404 if id not found */
+getHero(id: number): Observable<Hero> {
+  const url = `${this.heroesUrl}/${id}`;
+  return this.http.get<Hero>(url);
+}
+  // getHero(id: number): Observable<Hero> //mock-heroes.ts
+  // {
+  //   const hero = HEROES.find(h=>h.id===id)!;
+  //   return of(hero);
+  // }
  private log (message:string)
  {
   this.messageService.add(`HeroService: ${message}`);
  }
+ updateHero(hero: Hero) : Observable<any>
+ {
+  return this.http.put(this.heroesUrl, hero, this.httpOptions);
+ }
+ addHero(hero:Hero): Observable<Hero>{
+  return this.http.post<Hero>(this.heroesUrl, hero, this.httpOptions);
+ }
+ deleteHero(id: number) {
+ const url =`${this.heroesUrl}/${id}`;
+ return this.http.delete<Hero>(url, this.httpOptions);
+}
+searchHeroes(term:string): Observable<Hero[]>
+{
+  if(!term.trim())
+  {
+    return of([]);
+  }
+  return this.http.get<Hero[]>(`${this.heroesUrl}/?name=${term}`);
+}
 }
